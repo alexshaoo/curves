bce155e19f7dfe4f2ee0227f65c710530d9083b6
Alex S <qwshao@uwaterloo.ca>
2022-10-13 15:45:13 -0400

    updated constant FONT

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ STEP = 0.01
 WIDTH, HEIGHT = 1280, 720
 DIMENSIONS = (WIDTH, HEIGHT)
 FPS = 165
+FONT = pygame.font.SysFont("freesans", 20)
 
 pygame.init()
 screen = pygame.display.set_mode(DIMENSIONS)
@@ -32,21 +33,16 @@ class button():
   def draw(self, win, outline=None):
     if outline:
       pygame.draw.rect(win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
-
     pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
 
     if self.text != '':
-      font = pygame.font.SysFont('comicsans', 30)
-      text = font.render(self.text, 1, (0, 0, 0))
+      text = FONT.render(self.text, 1, (0, 0, 0))
       win.blit(text, 
       (self.x + (self.width/2 - text.get_width()/2), 
       self.y + (self.height/2 - text.get_height()/2)))
 
-  def isOver(self, pos):
-    if pos[0] > self.x and pos[0] < self.x + self.width:
-      if pos[1] > self.y and pos[1] < self.y + self.height:
-        return True
-    return False
+  def clicked(self, pos):
+    return self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height
 
 points = []
 knots = []
@@ -74,9 +70,7 @@ def redraw():
     curve_type = "Curve Type: Cubic Spline"
   else:
     curve_type = "Curve Type: None"
-
-  font = pygame.font.SysFont("freesans", 20)
-  curve_text = font.render(curve_type, True, BLACK)
+  curve_text = FONT.render(curve_type, True, BLACK)
   screen.blit(curve_text, (10, 10))
 
 def lagrange():
