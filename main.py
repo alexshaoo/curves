2da37decab6044d528ebbab0255cfab43ec2124e
Alex S <qwshao@uwaterloo.ca>
2022-10-13 15:33:40 -0400

    reformat

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class button():
 
   def draw(self, win, outline=None):
     if outline:
-      pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+      pygame.draw.rect(win, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
 
     pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
 
@@ -39,8 +39,8 @@ class button():
       font = pygame.font.SysFont('comicsans', 30)
       text = font.render(self.text, 1, (0, 0, 0))
       win.blit(text, 
-      (self.x + (self.width / 2 - text.get_width() / 2), 
-      self.y + (self.height / 2 - text.get_height() / 2)))
+      (self.x + (self.width/2 - text.get_width()/2), 
+      self.y + (self.height/2 - text.get_height()/2)))
 
   def isOver(self, pos):
     if pos[0] > self.x and pos[0] < self.x + self.width:
