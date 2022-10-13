286d355c4b02c2c81de36969240ce424b33114c6
Alex S <qwshao@uwaterloo.ca>
2022-10-13 04:01:11 -0400

    button class with mouse clicks implemented in main game loop

--- main.py
+++ main.py
@@ -20,6 +20,34 @@ pygame.display.set_caption("Curves")
 screen.fill(WHITE)
 clock = pygame.time.Clock()
 
+class button():
+  def __init__(self, color, x, y, width, height, text=''):
+    self.color = color
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.text = text
+
+  def draw(self, win, outline=None):
+    if outline:
+      pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+    pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+
+    if self.text != '':
+      font = pygame.font.SysFont('comicsans', 30)
+      text = font.render(self.text, 1, (0, 0, 0))
+      win.blit(text, 
+      (self.x + (self.width / 2 - text.get_width() / 2), 
+      self.y + (self.height / 2 - text.get_height() / 2)))
+
+  def isOver(self, pos):
+    if pos[0] > self.x and pos[0] < self.x + self.width:
+      if pos[1] > self.y and pos[1] < self.y + self.height:
+        return True
+    return False
+
 points = []
 knots = []
 
@@ -43,6 +71,14 @@ def bezier():
 
 
 run = True
+pressed = 0
+old_pressed = 0
+old_left_click = 0
+old_right_click = 0
+
+selected = -1
+bezier_button = button((0,255,0),650,200,120,70,"Bezier")
+
 while run:
   clock.tick(FPS)
   frameRate = int(clock.get_fps())
@@ -54,6 +90,17 @@ while run:
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_ESCAPE:
         run = False
+
+    pos = pygame.mouse.get_pos()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      pressed = -1
+      bezier()
+    elif event.type == pygame.MOUSEBUTTONUP:
+      pressed = 1
+    else:
+      pressed = 0
+
+
 
   pygame.display.update()
 pygame.quit()
